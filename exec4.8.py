c0c5c02b2638ee84c803e1d3ad3726c2bdfb6d89
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-27 20:23:50 -0300

    Update exec4.8.py

--- exec4.8.py
+++ exec4.8.py
@@ -3,9 +3,9 @@ texto = input("Digite uma frase com 5 palavras: ")
 palavras = nltk.word_tokenize(texto)
 
 while 
-if palavras > 5: 
-  print("Você digitou uma frase com mais de 5 palavras, digite uma frase com 5 palavras: ")
+  if palavras > 5: 
+    print("Você digitou uma frase com mais de 5 palavras, digite uma frase com 5 palavras: ")
 
-else: 
-  print (palavras) 
+  else: 
+    print (palavras) 
 
