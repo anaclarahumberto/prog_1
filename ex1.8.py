573dcadd640e1f3b072aad30ff7368f8c23f9631
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 15:15:29 -0300

    Update and rename ex1.8.py to exec1.8.py

--- ex1.8.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("Digite um número:"))
-
-raiz_quadrada = numero ** (1/2)
-print(f"A raiz quadrada do número é {raiz_quadrada:.1f}")
-
---
+++ exec1.8.py
@@ -0,0 +1,6 @@
+import math
+
+numero = int(input("Digite um número:"))
+raiz_quadrada = math.sqrt(numero)
+print(f"A raiz quadrada do número {numero} é: {raiz_quadrada}")
+
