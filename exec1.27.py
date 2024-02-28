c7c4e971565d85f359cdeb1a0f2604a8e882fe3b
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 16:35:41 -0300

    Update and rename ex1.27.py to exec1.27.py

--- exec1.27.py
+++ exec1.27.py
@@ -3,4 +3,4 @@ salario = float(input("Digite o seu salário: R$"))
 valor_imposto = float(input("Digite o valor do imposto: R$"))
 
 salario_liquido = salario - valor_imposto
-print(f"O seu salário líquido é de R${salario_liquido}")
+print(f"O seu salário líquido de {nome} é de R${salario_liquido}")
