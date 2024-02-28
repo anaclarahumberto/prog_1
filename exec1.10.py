bb79e8707abb870a4841e65b3c3e48fb9bb4a728
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 15:21:24 -0300

    Update and rename ex1.10.py to exec1.10.py

---
+++ exec1.10.py
@@ -0,0 +1,5 @@
+numero_1 = int(input("Digite o primeiro número:"))
+numero_2 = int(input("Digite o segundo número:"))
+
+potencia = numero_1 ** numero_2
+print(f"A potência do primeiro número pelo segundo é de {potencia}")
