b8ddb7ea19b6ca2dc491ad41f6566c813e181d25
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 20:22:45 -0300

    Update exec2.9.py

--- exec2.9.py
+++ exec2.9.py
@@ -4,7 +4,7 @@ mes = int(partes[0])
 dia = int(partes[1])
 ano = int(partes[2])
 
-if (dia >= 1 and dia <=31) and (mes >=1 and mes <=12) and ano >= 1000: 
+if (mes >=1 and mes <=12) and (dia >= 1 and dia <=31) and ano >= 1000: 
     print("É uma data válida!")
 else:
     print("Não é uma data válida!")
