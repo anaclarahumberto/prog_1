3d47d7b5b44a05d333ae898ddca4ee66e14e3947
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 16:20:11 -0300

    Update and rename 1.21.py to exec.1.21.py

--- exec.1.21.py
+++ exec.1.21.py
@@ -1,7 +1,7 @@
 comprimento_lado1=float(input("Digite o comprimento de um dos lados de um triangulo retângulo: "))
 comprimento_lado2=float(input("Digite o comprimento do outro lado:"))
 
-hipotenusa_aoquadrado= (comprimento_lado1**2) + (comprimento_lado2**2)
-hipotenusa= hipotenusa_aoquadrado*1/2
+hipotenusa_ao_quadrado= (comprimento_lado1**2) + (comprimento_lado2**2)
+hipotenusa= hipotenusa_ao_quadrado**0.5
 
 print(" A hipotenusa vale:",hipotenusa)
