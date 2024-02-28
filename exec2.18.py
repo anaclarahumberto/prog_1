ff72fafd06f1d0212458b68a609945182732ba91
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 17:23:51 -0300

    Update and rename ex2.18.py to exec2.18.py

--- exec2.18.py
+++ exec2.18.py
@@ -2,8 +2,10 @@ idade = int(input("Digite a sua idade:"))
 
 if idade < 30:
     print("Você é jovem!")
+    
 elif idade >=30 and idade <=60:
     print("Você é adulto!")
+    
 else:
     print("Você é idoso!")
     
