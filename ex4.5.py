676765248048dcd024f6a2d444fd6a290f8d1e21
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 19:37:40 -0300

    Update and rename ex4.5.py to exec4.5.py

--- ex4.5.py
+++
@@ -1,4 +0,0 @@
-import math
-numero = int(input("Digite um número inteiro:"))
-
-print(math.factorial(numero))
---
+++ exec4.5.py
@@ -0,0 +1,4 @@
+import math
+numero = int(input("Digite um número inteiro para calcular seu fatorial:"))
+
+print(math.factorial(numero))
