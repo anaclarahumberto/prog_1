c3aa8994a4302098bfa2886655461c36cefa2b06
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 16:33:22 -0300

    Update and rename ex1.26.py to exec1.26.py

---
+++ exec1.26.py
@@ -0,0 +1,6 @@
+cateto_1 = float(input("Digite o valor do primeiro cateto:"))
+cateto_2 = float(input("Digite o valor do segundo cateto:"))
+
+area = (cateto_1 * cateto_2) / 2 # area = b*h/2
+print(f"O valor da área do triângulo retângulo é de {area} cm²")
+
