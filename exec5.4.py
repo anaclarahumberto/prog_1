d9d42a645b14a54ce58899e81b07d37a5d3a64ac
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 20:33:42 -0300

    Update exec5.4.py

--- exec5.4.py
+++ exec5.4.py
@@ -1,3 +1,3 @@
-numeros =list(range(31))
-divisiveis = list(filter (lambda x: x%3==0 or x%5==0, numeros))
-print(divisiveis)
+numeros = list(range(31))
+divisiveis = list(filter(lambda x: x%3==0 or x%5==0, numeros ))
+print("Os números de 0 a 30, divisíveis por 3 ou 5 são:", divisiveis) 
