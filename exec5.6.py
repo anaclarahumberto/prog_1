b7dd659bd1aaeaffe2a0051e84b8fe3ce8f09709
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 20:57:52 -0300

    Update and rename Não confirmado 571945.crdownload to exec5.6.py

--- exec5.6.py
+++ exec5.6.py
@@ -2,4 +2,4 @@ import math
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 raiz_quadrada = {x: math.sqrt(x) for x in numeros}
 
-print(raiz_quadrada)
+print("As raizes_quadradas dos números são:",raiz_quadrada)
