f3fc83fc3baf1eabedd8e417f77bde763eb0053a
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 16:42:49 -0300

    Update and rename ex1.30.py to exec1.30.py

---
+++ exec1.30.py
@@ -0,0 +1,8 @@
+conjunto_1 = {0, 1, 2, 3, 4}
+conjunto_2 = {5, 6, 7, 8, 9}
+# União
+print(conjunto_1 | conjunto_2)
+# Interseção
+print(conjunto_1 & conjunto_2)
+# Diferença
+print(conjunto_1 - conjunto_2)
