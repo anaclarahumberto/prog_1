9758d2d7d2b7222d6863cdbc39d072884dacde64
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 15:35:59 -0300

    Update and rename ex1.12.py to exec1.12.py

--- exec1.12.py
+++ exec1.12.py
@@ -2,4 +2,4 @@ raio = float(input("Digite o raio do círculo:"))
 
 area = 3.14 * (raio**2)
 comprimento = 2 * 3.14 * raio
-print(f"A área do círculo é {area} e o seu comprimento é {comprimento}")
+print(f"A área do círculo é: {area} e o seu comprimento é: {comprimento}")
