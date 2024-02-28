9def1433a35d334e503c7ab79d3fd60d18ac1cde
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 18:08:31 -0300

    Update and rename ex3.16.py to exec3.16.py

--- exec3.16.py
+++ exec3.16.py
@@ -10,7 +10,7 @@ maior_numero = max(numeros)
 menor_numero = min(numeros)
 
 print(f"O maior número é {maior_numero}")
-print(f"O menor número é {menor_numero}:")
+print(f"O menor número é {menor_numero}")
 
     
     
