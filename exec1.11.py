2fa144699b7bb004a9d1272a8d41a5a3f2964316
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 15:25:13 -0300

    Update and rename ex1.11.py to exec1.11.py

--- exec1.11.py
+++ exec1.11.py
@@ -1,7 +1,7 @@
-numero1 = int(input("Digite o primeiro número (a):"))
-numero2 = int(input("Digite o segundo número (b):"))
-numero3 = int(input("Digite o terceiro número (c):"))
-# (b^2) - 4*a*c
+numero1 = int(input("Digite um número para (a):"))
+numero2 = int(input("Digite um número (b):"))
+numero3 = int(input("Digite um número (c):"))
+
 delta = (numero2**2) - 4 * numero1 * numero3
 x1 = ((-1*numero2) + (delta **(1/2))) / (2*numero1)
 x2 = ((-1*numero2) - (delta **(1/2))) / (2*numero1)
