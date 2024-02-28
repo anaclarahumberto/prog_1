511977e5709c1f395e6dddd59252788bb750875d
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 17:17:30 -0300

    Update and rename ex2.14.py to exec2.14.py

---
+++ exec2.14.py
@@ -0,0 +1,7 @@
+numero_1 = int(input("Digite um número inteiro:"))
+numero_2 = int(input("Digite outro número inteiro:"))
+
+if numero_1 % numero_2 == 0:
+    print("É divisível!")
+else:
+    print("Não é divisível!")
