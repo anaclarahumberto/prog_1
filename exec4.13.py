f4a390542aa55d892c8df4f20c1dafccea71fd1e
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 20:49:41 -0300

    Update and rename Não confirmado 328668.crdownload to exec4.13.py

--- exec4.13.py
+++ exec4.13.py
@@ -2,4 +2,4 @@ frase = input("Digite uma frase: ")
 palavras = frase.split()
 palavras_sem_espacos = [palavra.strip() for palavra in palavras]
 frase_com_um_espaco = " ".join(palavras_sem_espacos)
-print(frase_com_um_espaco)
+print("A frase com somente 1 espaço ficou:",frase_com_um_espaco)
