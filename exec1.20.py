4c9d934964eb13a9ff6f2abd430e106e84c8e94b
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 16:17:19 -0300

    Update exec1.20.py

--- exec1.20.py
+++ exec1.20.py
@@ -1,11 +1,6 @@
-nome_produto = input("Digite o nome do produto:")
-preco_custo = float(input("Digite o preço de custo do produto:"))
-preco_venda = float(input("Digite o preço de venda do produto:"))
-quantidade_produto_estoque = int(input("Digite a quantidade do produto em estoque:"))
+angulo_rad=float(input("Digite a média de um ãngulo em radiano( sem o pi ): "))
 
-lucro = preco_venda - preco_custo
-lucro_total = lucro * quantidade_produto_estoque
-print(f"O lucro gerado por esse produto na queima de estoque é de R${lucro_total}")
+grau= 180*angulo_rad
 
-
+print("O valor em graus é:",grau)
                     
