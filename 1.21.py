768a21974a0ac7ff0b82d4d0f2c1e492431cd9d3
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 16:18:05 -0300

    Add files via upload

---
+++ 1.21.py
@@ -0,0 +1,7 @@
+comprimento_lado1=float(input("Digite o comprimento de um dos lados de um triangulo retângulo: "))
+comprimento_lado2=float(input("Digite o comprimento do outro lado:"))
+
+hipotenusa_aoquadrado= (comprimento_lado1**2) + (comprimento_lado2**2)
+hipotenusa= hipotenusa_aoquadrado*1/2
+
+print(" A hipotenusa vale:",hipotenusa)
---
+++ 1.22.py
@@ -0,0 +1,6 @@
+distancia_percorrida=float(input("Digite a distancia percorrida pelo objeto: "))
+tempo_gasto=float(input("Digite o tempo gasto: "))
+
+velocidade_media= distancia_percorrida / tempo_gasto
+
+print("A velocidade média vale: ",velocidade_media)
