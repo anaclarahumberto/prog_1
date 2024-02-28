0bfd703970b346c5e063415cff110022a5a9340f
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 16:09:42 -0300

    Update and rename ex1.19.py to exec1.19.py

--- exec1.19.py
+++ exec1.19.py
@@ -5,5 +5,5 @@ tempo_transicao = float(input("Digite o tempo de transição entre as velocidade
 velocidade_inical_ms = velocidade_inical * 1000 / 3600
 velocidade_final_ms = velocidade_final * 1000 / 3600
 aceleracao = (velocidade_final_ms - velocidade_inical_ms) / (tempo_transicao - 0)
-print(f"A aceleração é de {aceleracao:.2f} m/s²")
+print(f"A aceleração é de: {aceleracao:.2f} m/s²")
 
