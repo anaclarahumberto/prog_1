81ca21dffe241cc20185b6c06a55ef2f602d7a68
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 20:04:05 -0300

    Update and rename Não confirmado 550632.crdownload to exec4.10.py

--- exec4.10.py
+++ exec4.10.py
@@ -3,4 +3,4 @@ artigos = ["o", "a", "os", "as", "um", "uma", "uns", "umas"]
 palavras = frase.split()
 palavras_sem_artigos = [palavra for palavra in palavras if palavra.lower() not in artigos]
 frase_sem_artigos = " ".join(palavras_sem_artigos)
-print("Frase sem artigos:", frase_sem_artigos)
+print("A frase sem artigos ficou:", frase_sem_artigos)
