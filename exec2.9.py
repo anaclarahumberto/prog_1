8a67f6679b4d39ee5dfdf2afaecef320ad5766dd
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 19:54:47 -0300

    Update exec2.9.py

--- exec2.9.py
+++ exec2.9.py
@@ -1,7 +1,7 @@
 data = input("Digite a data (mm/dd/aaaa): ")
 partes = data.split('/')
-dia = int(partes[0])
-mes = int(partes[1])
+mes = int(partes[0])
+dia = int(partes[1])
 ano = int(partes[2])
 
 if (dia >= 1 and dia <=31) and (mes >=1 and mes <=12) and ano >= 1000: 
