59ac2ca5da3ba7e9bbdbadc05e34ac2ac06818e5
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 16:40:48 -0300

    Update and rename ex1.29.py to exec1.29.py

---
+++ exec1.29.py
@@ -0,0 +1,6 @@
+nome_familiares = {"nome": "Ana", "nome2": "Ellen", "nome3": "Luciana", "nome4": "Elson", "nome5": "Noah"}
+cor_olho = {"olho": "castanho claro", "olho2": "castanho claro", "olho3": "castanho escuro", "olho4": "verde", "olho5": "castanho escuro",}
+idade = {"idade": 18, "idade2": 19, "idade3": 46, "idade4": 68, "idade5": 1,}
+print(nome_familiares)
+print(cor_olho)
+print(idade)
