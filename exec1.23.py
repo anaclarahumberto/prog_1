2afbbe29c5f877846d00199bf3abc64aae66a42f
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-29 07:29:29 -0300

    Update exec1.23.py

--- exec1.23.py
+++ exec1.23.py
@@ -2,6 +2,6 @@ distancia_percorrida = float(input("Digite a distância percorrida pelo objeto e
 tempo_gasto = float(input("Digite o tempo gasto pelo objeto em (s):"))
 aceleracao = float(input("Digite a aceleração em (m/s²):"))
 
-velocidade_inicial = (aceleracao / tempo_gasto) - ((aceleracao * tempo_gasto) / 2)
+velocidade_inicial = distancia/tempo - aceleracao * tempo / 2 
 velocidade_final = velocidade_inicial + aceleracao * tempo_gasto
 print("A velocidade inicial do objeto é ", velocidade_inicial, "m/s², e a sua velocidade final é de ",velocidade_final, "m/s².")
