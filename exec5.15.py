783117b529a697fad8388064cbaebbc390f31491
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-02-28 21:25:05 -0300

    Update exec5.15.py

--- exec5.15.py
+++ exec5.15.py
@@ -2,6 +2,6 @@ numeros = [float(x) for x in input("Digite uma lista de números separados por e
 escolha = float(input("Digite um número de referência: "))
 posicao = next((i for i, num in enumerate(numeros) if num > escolha), None)
 if posicao is not None:
-    print(f"A posição do primeiro elemento maior que {escolha} é {posicao}")
+    print("A posição do primeiro elemento maior que",escolha,"é:",posicao)
 else:
-    print(f"Nenhum elemento na lista é maior que {escolha}")
+    print("Nenhum elemento na lista é maior que:", escolha)
